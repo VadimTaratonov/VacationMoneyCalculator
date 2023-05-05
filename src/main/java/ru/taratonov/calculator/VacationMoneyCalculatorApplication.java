03197a3f6b06ecf9b90d645f594b9fb4714fa6ca
Vadim Taratonov <taratonovv8@gmail.com>
2023-05-05 16:50:39 +0300

    Initialize project

---
+++ src/main/java/ru/taratonov/calculator/VacationMoneyCalculatorApplication.java
@@ -0,0 +1,13 @@
+package ru.taratonov.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationMoneyCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VacationMoneyCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/taratonov/calculator/VacationMoneyCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.taratonov.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationMoneyCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
