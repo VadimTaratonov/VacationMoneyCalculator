9eeffdf7337cfb049f4ee308d2a955504cca621b
Vadim Taratonov <taratonovv8@gmail.com>
2023-05-08 19:18:52 +0300

    Add controller advice for catching exceptions

---
+++ src/main/java/ru/taratonov/calculator/controller/ControllerAdvice.java
@@ -0,0 +1,24 @@
+package ru.taratonov.calculator.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.taratonov.calculator.dto.ErrorDto;
+import ru.taratonov.calculator.exception.InvalidDateRangeException;
+import ru.taratonov.calculator.exception.NumberOfVacationDaysException;
+import ru.taratonov.calculator.exception.ValueLessThanZeroException;
+import ru.taratonov.calculator.exception.WrongYearException;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class ControllerAdvice {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({ValueLessThanZeroException.class, NumberOfVacationDaysException.class,
+            InvalidDateRangeException.class, WrongYearException.class})
+    public ErrorDto handleNotFoundException(Exception ex) {
+        return new ErrorDto(ex.getMessage(), LocalDateTime.now(), HttpStatus.BAD_REQUEST);
+    }
+}
