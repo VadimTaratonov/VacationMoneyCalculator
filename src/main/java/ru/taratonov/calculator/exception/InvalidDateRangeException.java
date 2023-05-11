e0565eab33834f140b41d3ab2873d741486058f9
Vadim Taratonov <taratonovv8@gmail.com>
2023-05-11 17:26:23 +0300

    Refactor message

--- src/main/java/ru/taratonov/calculator/exception/InvalidDateRangeException.java
+++ src/main/java/ru/taratonov/calculator/exception/InvalidDateRangeException.java
@@ -3,6 +3,6 @@ package ru.taratonov.calculator.exception;
 public class InvalidDateRangeException extends RuntimeException {
     @Override
     public String getMessage() {
-        return "Your finish date of vacation is before the start date ";
+        return "Your finish date of vacation is before the start date!";
     }
 }
